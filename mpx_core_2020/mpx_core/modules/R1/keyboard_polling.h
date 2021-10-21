512b5b26ea5fb4915c896b945571cdd2168f4d6b
MatthewAngeline <matthewangeline@mba0004.internal>
2021-10-20 21:01:49 -0400

    Deleted the extra main file

--- mpx_core_2020/mpx_core/modules/R1/keyboard_polling.h
+++
@@ -1,7 +0,0 @@
-#ifndef _KEYBOARD_POLLING_H
-#define _KEYBOARD_POLLING_H
-
-
-int *keyboard_polling(char * buffer, int * count);
-
-#endif
--- mpx_core_2020/mpx_core/modules/R4/infiniteCommands.h
+++
@@ -1,6 +0,0 @@
-#ifndef _INFINITECOMMANDS_H
-#define _INFINITECOMMANDS_H
-
-void foreverIdle();
-#endif
-
